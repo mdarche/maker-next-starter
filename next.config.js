1184fecff81267b0ccca9e92850b1286b0e8dc8a
mdarche <mkdarche@gmail.com>
2023-04-30 10:00:55 -0400

    chore: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const config = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
 }
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer(config)
